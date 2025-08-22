903f65eb6728abcdf30eb950fcf738ddb5dfd613
Thomas Martinez <devtommie114@gmail.com>
2025-08-22 16:33:46 -0400

    restarted project with tailwind4 - added header and footer

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '@/assets/styles/globals.css';
+import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from '@/lib/constants';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en' suppressHydrationWarning>
+      <body className={`${inter.className}`}>{children}</body>
+    </html>
+  );
+}
